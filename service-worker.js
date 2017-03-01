ba8ac8cc1f5b20ff95637de062b9f5da7367e168
nolaandy <andyw504@gmail.com>
2017-02-28 22:34:18 -0600

    New Repo

--- service-worker.js
+++ service-worker.js
@@ -1,66 +1,28 @@
-// Copyright 2016 Google Inc.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+let cahceTimestamp = '1486086904919'
+let cacheName = 'nvduoc-shell-v' + cahceTimestamp
+var dataCacheName = 'nvduoc-data-v' + cahceTimestamp
+let filesToCache = [
+  '/'
+]
 
-var dataCacheName = 'AndyzData';
-var cacheName = 'AndyzPWA';
-var filesToCache = [
-  './index.html',
-  './static/css/app.cbd4cc70d0d31152559050637fe14cad.css',
-  './static/js/manifest.76dfcc9f56ff9d6eef4f.js',
-  './static/js/vendor.75de5f651352b827b21e.js',
-  './static/js/app.a8706876673ed61e6968.js',
-  './favicon.ico'
-];
-
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Install');
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('activate', function(e) {
+self.addEventListener('activate', (e) => {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
         if (key !== cacheName && key !== dataCacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
-      }));
+      }))
     })
-  );
-  /*
-   * Fixes a corner case in which the app wasn't returning the latest data.
-   * You can reproduce the corner case by commenting out the line below and
-   * then doing the following steps: 1) load app for first time so that the
-   * initial New York City data is shown 2) press the refresh button on the
-   * app 3) go offline 4) reload the app. You expect to see the newer NYC
-   * data, but you actually see the initial data. This happens because the
-   * service worker is not yet activated. The code below essentially lets
-   * you activate the service worker faster.
-   */
-  return self.clients.claim();
-});
+  )
+  return self.clients.claim()
+})
 
 self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
+  // console.log('[Service Worker] Fetch', e.request.url)
+  let dataUrl = '/wp-json/wp/v2'
   if (e.request.url.indexOf(dataUrl) > -1) {
     /*
      * When the request URL contains dataUrl, the app is asking for fresh
@@ -70,13 +32,13 @@ self.addEventListener('fetch', function(e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
      */
     e.respondWith(
-      caches.open(dataCacheName).then(function(cache) {
-        return fetch(e.request).then(function(response){
-          cache.put(e.request.url, response.clone());
-          return response;
-        });
+      caches.open(dataCacheName).then((cache) => {
+        return fetch(e.request).then((response) => {
+          cache.put(e.request.url, response.clone())
+          return response
+        })
       })
-    );
+    )
   } else {
     /*
      * The app is asking for app shell files. In this scenario the app uses the
@@ -84,9 +46,28 @@ self.addEventListener('fetch', function(e) {
      * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
      */
     e.respondWith(
-      caches.match(e.request).then(function(response) {
-        return response || fetch(e.request);
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request).then(function(response) {
+            if ((e.request.url.indexOf('.js') > -1) || (e.request.url.indexOf('.css') > -1)) {
+              return caches.open(cacheName).then(function(cache) {
+                cache.put(e.request, response.clone());
+                return response;
+              });
+            }else{
+              return response;
+            }
+          });
       })
     );
   }
 });
+
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install')
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[ServiceWorker] Caching app shell')
+      return cache.addAll(filesToCache)
+    })
+  )
+})
